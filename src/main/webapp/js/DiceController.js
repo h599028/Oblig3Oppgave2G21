40ebd12fe8680e8a47d58a615005d0fab19191cf
Mathias <599028@stud.hvl.no>
2022-10-22 12:35:43 +0200

    ctrl+shift+f

--- src/main/webapp/js/DiceController.js
+++ src/main/webapp/js/DiceController.js
@@ -1,38 +1,38 @@
 
 class Dice {
-    #resultat;
+	#resultat;
 
-    constructor() {
-        this.#resultat =0;
-    }
+	constructor() {
+		this.#resultat = 0;
+	}
 
-    roll() {
-        let resultat = Math.floor(Math.random() * 6) + 1;
-        document.getElementById("resultat").innerHTML = resultat;
-    }
+	roll() {
+		let resultat = Math.floor(Math.random() * 6) + 1;
+		document.getElementById("resultat").innerHTML = resultat;
+	}
 
 }
 
 class DiceController {
 
-    dice = new Dice();
+	dice = new Dice();
 
-    constructor (rootElement) {
-        this.rootElement = rootElement;
+	constructor(rootElement) {
+		this.rootElement = rootElement;
 
-        const dicebutton = rootElement.querySelector("*[data-dicebutton]");
-        dicebutton.addEventListener("click",this.#rollDice.bind(this));
-    }
+		const dicebutton = rootElement.querySelector("*[data-dicebutton]");
+		dicebutton.addEventListener("click", this.#rollDice.bind(this));
+	}
 
-    #rollDice() {
-        this.dice.roll()
-    }
+	#rollDice() {
+		this.dice.roll()
+	}
 
 }
 
 function init() {
-    const rootElement = document.getElementById("root");
-    new DiceController(rootElement);
+	const rootElement = document.getElementById("root");
+	new DiceController(rootElement);
 }
 
 document.addEventListener('DOMContentLoaded', init)
